9702bb64bd085d3fed6dfc47d7ef0daeec15194b
Mike Batanian <mikebatanian@gmail.com>
2013-02-25 20:58:18 -0500

    clean up

--- rToPocket.py
+++ rToPocket.py
@@ -7,8 +7,4 @@ if len(sys.argv) != 2:
 	print 'Invalid # of arguments'
 	print '  ./rToPocket {runner.py}'
 
-addFunc = imp.load_source('adder', sys.argv[1])
-print addFunc
-
-# adder = __import__(sys.argv[1], 'addItems')
-# print adder
+imp.load_source('adder', sys.argv[1])
